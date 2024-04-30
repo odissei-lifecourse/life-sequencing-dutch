053365b54b2beb9ee659deb53c193306bf37bf07
tanzir5 <tanzir2430@gmail.com>
2024-04-30 11:51:46 -0400

    subsets dataset

--- src/llm/src/new_code/pretrain.py
+++ src/llm/src/new_code/pretrain.py
@@ -68,6 +68,32 @@ def get_callbacks(ckpoint_dir, val_check_interval):
   ]
   return callbacks
 
+<<<<<<< HEAD:src/llm/src/new_code/pretrain.py
+=======
+def get_train_val_dataloaders(dataset, batch_size, train_split=0.8, shuffle=True):
+  total_samples = len(dataset)
+  train_size = int(train_split * total_samples)
+  val_size = total_samples - train_size
+
+  train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+  return (
+    DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=71),
+    DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=71)
+  )
+
+def subset(data, lim):
+  for key in data:
+    data[key] = data[key][:lim]
+  # data["input_ids"] = data["input_ids"][:lim]
+  # data["padding_mask"] = data["padding_mask"][:lim]
+  # data["original_sequence"] = data["original_sequence"][:lim]
+  # data["target_tokens"] = data["target_tokens"][:lim]
+  # data["target_pos"] = data["target_pos"][:lim]
+  # data["target_cls"] = data["target_cls"][:lim]
+  return data
+
+>>>>>>> e9cd94c (subsets dataset):code/llm/src/new_code/pretrain.py
 def pretrain(cfg):
   hparams_path = cfg['HPARAMS_PATH']#'src/new_code/regular_hparams.txt'
   ckpoint_dir = cfg['CHECKPOINT_DIR']
@@ -89,6 +115,7 @@ def pretrain(cfg):
   else:
     model = TransformerEncoder(hparams)
   
+<<<<<<< HEAD:src/llm/src/new_code/pretrain.py
   callbacks = get_callbacks(ckpoint_dir, val_check_interval+1)
   #ddp = DDPStrategy(process_group_backend="mpi")
   logger = CSVLogger(ckpoint_dir)
@@ -119,6 +146,50 @@ def pretrain(cfg):
   print_now("training and validation dataloaders are created")
   trainer.fit(model, train_dataloader, val_dataloader)
   
+=======
+  mlm_paths = []
+  for root, dirs, files in os.walk(mlm_dir):
+    for file_path in files:
+      mlm_paths.append(os.path.join(root, file_path))
+
+  LIM = 1000
+  val_dataloader = None
+  batch_size = cfg['BATCH_SIZE']
+  for epoch in range(cfg['MAX_EPOCHS']):
+    for counter, mlm_path in enumerate(mlm_paths):
+      if counter == 0:
+        if val_dataloader is None:
+          with open(mlm_path, 'rb') as f:
+            dataset = pickle.load(f)
+            dataset.data = subset(dataset.data, LIM)
+          val_dataloader = DataLoader(dataset, batch_size=batch_size)
+        continue
+      with open(mlm_path, 'rb') as f:
+        dataset = pickle.load(f)
+        dataset.data = subset(dataset.data, LIM)
+      callbacks = get_callbacks(ckpoint_dir, counter)
+      trainer = Trainer(
+        default_root_dir=ckpoint_dir,
+        callbacks=callbacks,
+        max_epochs=1,
+        accelerator='gpu',
+        devices=1
+      )
+      # Create a data loader
+      # train_dataloader, val_dataloader = get_train_val_dataloaders(
+      #   dataset=dataset,
+      #   batch_size=batch_size,
+      #   train_split=hparams['train_split']
+      # )
+      train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+      print_now("training and validation dataloaders are created")
+      print_now(f"# of batches in training: {len(train_dataloader)}")
+      print_now(f"# of batches in validation: {len(val_dataloader)}")
+      trainer.fit(model, train_dataloader)
+      trainer.validate(model, val_dataloader)
+
+>>>>>>> e9cd94c (subsets dataset):code/llm/src/new_code/pretrain.py
 if __name__ == "__main__":
   torch.set_float32_matmul_precision("medium")
   CFG_PATH = sys.argv[1]
