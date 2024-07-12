e2dfc3e7417cc56739718aec64a5a512c8564025
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-07-12 14:58:56 +0200

    add slurm script

---
+++ src/evaluation/slurm_scripts/isolate_evaluation_subsets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --job-name=isolate_evaluation_subset
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=0
+#SBATCH -p comp_env
+#SBATCH -e /gpfs/ostor/ossc9424/homedir/logs/%x.%j.err
+#SBATCH -o /gpfs/ostor/ossc9424/homedir/logs/%x.%j.err
+
+cd /gpfs/ostor/ossc9424/homedir/Life_Course_Evaluation/
+
+module purge 
+source ossc_env/bin/activate
+module load 2022 
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-intel-2022a
+
+cd /gpfs/ostor/ossc9424/homedir/Life_Course_Evaluation/
+
+date
+echo "Starting scripts"
+time python isolate_income_subset.py
+time python isolate_marriage_subset.py 
+time python extract_embedding_subset.py
+
+echo "job ended" 
