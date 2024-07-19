c3149aa85805a35e262520f0083e608c4bb9e7e2
Flavio Hafner <hafner.flavio@gmail.com>
2024-07-19 11:54:27 +0200

    Update pipeline/json seq for snellius
    - both the slurm scripts and the configs
    - update SBATCH args to actual usage of memory/time

--- src/llm/slurm_scripts/pipeline.sh
+++ src/llm/slurm_scripts/pipeline.sh
@@ -2,25 +2,61 @@
 #
 #SBATCH --job-name=pipeline
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 72
+#SBATCH --cpus-per-task 4 
 #SBATCH --nodes=1
-#SBATCH --time=12:00:00
-#SBATCH --mem=900G
-#SBATCH -p comp_env
-#SBATCH -e /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/%x.%j.err
-#SBATCH -o /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/%x.%j.out
+#SBATCH --time=02:30:00
+#SBATCH --mem=20G
+#SBATCH -p fat_rome
+#SBATCH -e %x-%j.err
+#SBATCH -o %x-%j.out
 
-echo "job started"
 
-cd /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/ 
+# function to check if $2 is in $1
+stringContain() { case $2 in *$1* ) return 0;; *) return 1;; esac ;}
 
-module load 2022
-module load Python/3.10.4-GCCcore-11.3.0
+# This function can be called with `source script.sh` -> fast initialization during interactive jobs
+initialize() {
+	# note that `declare` are local variables and will not be available out of function scope
 
-source ossc_env_may2/bin/activate 
+	if stringContain "ossc" $USER; then
+	    declare DATADIR="/gpfs/ostor/ossc9424/homedir/data"
+	else
+	    declare DATADIR="/projects/0/prjs1019"
+	fi 
+	
+	# add more users here as necessary
+	if stringContain "ossc" $USER; then
+	    declare ROOTDIR="/gpfs/ostor/ossc9424/homedir"
+	    declare VENV="$ROOTDIR/ossc_env"
+	elif stringContain "fhafner" $USER; then 
+	    declare ROOTDIR="/gpfs/home4/$USER"
+	    declare REPO_DIR="$ROOTDIR/repositories/life-sequencing-dutch"
+	    declare VENV="$REPO_DIR/.venv"
+	fi
+	
+	module purge 
+	module load 2022 
+	module load Python/3.10.4-GCCcore-11.3.0
+	module load SciPy-bundle/2022.05-foss-2022a
+        module load h5py/3.7.0-foss-2022a
+	source "$VENV/bin/activate" 
+	
+	echo "job started" 
+	cd $REPO_DIR/src/llm/
+}
 
-date
-srun python -m src.new_code.pipeline projects/dutch_real/pipeline_cfg.json
+#cd /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/ 
 
-echo "job ended"
+#source ossc_env_may2/bin/activate 
+
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    initialize
+else
+    initialize
+
+    date
+    srun python -m src.new_code.pipeline projects/dutch_real/pipeline_cfg.json
+
+    echo "job ended"
+fi
 
