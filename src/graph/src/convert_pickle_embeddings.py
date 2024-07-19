500c4db80f2ad664e72f715ac2dc2d815ab2f61b
dakota0064 <dakota.handzlik@stonybrook.edu>
2024-07-19 09:47:31 -0400

    Fixed to read old embeddings correctly

--- src/graph/src/convert_pickle_embeddings.py
+++ src/graph/src/convert_pickle_embeddings.py
@@ -2,15 +2,31 @@ import h5py
 import pickle
 import numpy as np
 
-def convert_file(filename):
+def convert_file(filename, mapping_url):
 
-    # First load the embeddings from the old pickle file
+    embeddings = []
+    ids = []
+
+    # First get mappings
+    with open(mapping_url, 'rb') as pkl_file:
+        mappings = dict(pickle.load(pkl_file))
+
+    inverse_mappings = {}
+    for key, value in mappings.items():
+        inverse_mappings[value] = key
+
     with open(filename, 'rb') as pkl_file:
-        # Assumes we have a dictionary where keys are RINPERSOON ids and values are embedding arrays
-        data_dict = dict(pickle.load(pkl_file))
+        data = pickle.load(pkl_file)
 
-    ids = np.array(data_dict.keys())
-    embeddings = np.array(data_dict.values())
+    for i in range(len(data)):
+        embedding = data[i]
+        RINPERSOON = int(inverse_mappings[i])
+
+        embeddings.append(embedding)
+        ids.append(RINPERSOON)
+
+    ids = np.array(ids)
+    embeddings = np.array(embeddings)
 
     filename = filename.split(".emb")[0]
     filename += '.h5'
