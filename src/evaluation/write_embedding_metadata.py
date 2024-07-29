f5b4c7a761b05f69333beeedbe6e499013817c39
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-07-29 12:20:05 +0200

    update embedding meta

--- src/evaluation/write_embedding_metadata.py
+++ src/evaluation/write_embedding_metadata.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
              "type": "NET",
              "root": eval_root,
              "mapping": "/gpfs/ostor/ossc9424/homedir/Dakota_network/mappings/family_2010.pkl",
-             "url": "lr_steve_full_network_2010.h5",
+             "url": "lr_steve_full_network_2010_30.h5",
              "truth": "full"
              },
 
@@ -69,94 +69,130 @@ if __name__ == "__main__":
              }
         ]
 
-    # omitted gron
-    elif args.collection_name == "llm_embedding_set":
-
+    elif args.collection_name == "llm_tanzir_eval_set":
         embedding_sets = [
-
-            # {"name": "LLM Mean 2016 d64",
-            #  "year": "2016",
-            #  "type": "LLM",
-            #  "root": llm_emb_root,
-            #  "url": "mean_embedding_2017_64.h5",
-            #  "truth": "full"
-            #  },
-
-            # {"name": "LLM CLS 2017 small",
-            #  "year": "2016",
-            #  "type": "LLM",
-            #  "root": llm_emb_root,
-            # # "url": 2017_small.h5,
-            #  "url": "2017_small_v0.0.1.h5",
-            #  "emb_type": "cls_emb",
-            #  "truth": "full"
-            #  },
-
-            {"name": "LLM CLS 2017 medium",
-             "year": "2016",
+            {"name": "LLM Tanzir, eval sample",
+             "year": 2016,
              "type": "LLM",
-             "root": llm_emb_root,
-             "url": "2017_medium.h5",
-             # "url": "2017_medium_v0.0.1.h5",
+             "root": eval_root, 
+             "url": "embedding_2017_h64.h5",
              "emb_type": "cls_emb",
              "truth": "full"
-             },
+             }
+        ]    
 
-
-            {"name": "LLM CLS 2017 medium2x",
-             "year": "2016",
+    elif args.collection_name == "llm_tanzir": # untested
+        embedding_sets = [
+            {"name": "LLM Tanzir, full sample",
+             "year": 2016,
              "type": "LLM",
-             "root": llm_emb_root,
-             "url": "2017_medium2x.h5",
-             # "url": "2017_medium2x_v0.0.1.h5",
-             "emb_type": "cls_emb",
-             "truth": "full"
-             },
-
-            {"name": "LLM CLS 2017 large",
-             "year": "2016",
-             "type": "LLM",
-             "root": llm_emb_root,
-             "url": "2017_large.h5",
-             # "url": "2017_large_v0.0.1.h5",
-             "emb_type": "cls_emb",
+             "root": llm_emb_root_old, 
+             "url": "cls_embedding_2016.json",
              "truth": "full"
              }
         ]
 
-    elif args.collection_name == "llm_eval_set":
+    elif args.collection_name in ["llm_embedding_set", "llm_embedding_set_old"]: 
+        suffix = ""
+        if args.collection_name == "llm_embedding_set_old":
+            suffix = "_v0.0.1"
 
         embedding_sets = [
-            {"name": "LLM CLS 2017 medium",
-             "year": "2016",
-             "type": "LLM",
-             "root": eval_root,
-             "url": "2017_medium.h5",
-             # "url": "2017_medium_v0.0.1.h5",
-             "emb_type": "cls_emb",
-             "truth": "full"
-             },
 
-            {"name": "LLM CLS 2017 medium2x",
-             "year": "2016",
-             "type": "LLM",
-             "root": eval_root,
-             "url": "2017_medium2x.h5",
-             # "url": "2017_medium2x_v0.0.1.h5",
-             "emb_type": "cls_emb",
-             "truth": "full"
-             },
-
-            {"name": "LLM CLS 2017 large",
-             "year": "2016",
-             "type": "LLM",
-             "root": eval_root,
-             "url": "2017_large.h5",
-             # "url": "2017_large_v0.0.1.h5",
-             "emb_type": "cls_emb",
-             "truth": "full"
-             }
+            {
+                "name": "LLM CLS 2017 medium",
+                "year": 2016, 
+                "type": "LLM",
+                "root": llm_emb_root, 
+                "url": f"2017_medium{suffix}.h5",
+                "emb_type": "cls_emb",
+                "truth": "full"
+            },           
+            
+            {
+                "name": "LLM CLS 2017 medium2x",
+                "year": 2016, 
+                "type": "LLM",
+                "root": llm_emb_root, 
+                "url": f"2017_medium2x{suffix}.h5",
+                "emb_type": "cls_emb",
+                "truth": "full"
+            },
+            
+            {
+                "name": "LLM CLS 2017 large",
+                "year": 2016, 
+                "type": "LLM",
+                "root": llm_emb_root, 
+                "url": f"2017_large{suffix}.h5",
+                "emb_type": "cls_emb",
+                "truth": "full"
+            }
         ]
+        if args.collection_name == "llm_embedding_set_old":
+            add_set = {
+                "name": "LLM CLS 2017 small",
+                "year": 2016,
+                "type": "LLM",
+                "root": llm_emb_root,
+                "url": f"2017_small{suffix}.h5",
+                "emb_type:": "cls_emb",
+                "truth": "full"
+            }
 
+            embedding_sets.insert(0, add_set)
+
+
+    elif args.collection_name in ["llm_eval_set", "llm_eval_set_old"]: 
+        suffix = ""
+        if args.collection_name == "llm_eval_set_old":
+            suffix = "_v0.0.1"
+
+        embedding_sets = [
+
+            {
+                "name": "LLM CLS 2017 medium",
+                "year": 2016, 
+                "type": "LLM",
+                "root": eval_root, 
+                "url": f"2017_medium{suffix}.h5",
+                "emb_type": "cls_emb",
+                "truth": "full"
+            },           
+            
+            {
+                "name": "LLM CLS 2017 medium2x",
+                "year": 2016, 
+                "type": "LLM",
+                "root": eval_root, 
+                "url": f"2017_medium2x{suffix}.h5",
+                "emb_type": "cls_emb",
+                "truth": "full"
+            },
+            
+            {
+                "name": "LLM CLS 2017 large",
+                "year": 2016, 
+                "type": "LLM",
+                "root": eval_root, 
+                "url": f"2017_large{suffix}.h5",
+                "emb_type": "cls_emb",
+                "truth": "full"
+            }
+        ]
+        if args.collection_name == "llm_eval_set_old":
+            add_set = {
+                "name": "LLM CLS 2017 small",
+                "year": 2016,
+                "type": "LLM",
+                "root": eval_root,
+                "url": f"2017_small{suffix}.h5",
+                "emb_type:": "cls_emb",
+                "truth": "full"
+            }
+
+            embedding_sets.insert(0, add_set)
+
+        
     with open(save_url, "wb") as pkl_file:
         pickle.dump(embedding_sets, pkl_file)
