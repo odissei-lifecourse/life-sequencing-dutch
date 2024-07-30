5f37c3dee00e953016c28c6f2618e216350fef7b
benjamic <benjamin.czaja@surf.nl>
2024-07-30 15:58:06 +0200

    cleaning up jobscript

--- src/llm/slurm_scripts/pretrain_multigpu_singlenode.sh
+++ src/llm/slurm_scripts/pretrain_multigpu_singlenode.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH -p gpu_sw
-#SBATCH --gpus-per-node=4
-#SBATCH --exclusive
+#SBATCH --job-name=pretrain
+#SBATCH -p gpu
+#SBATCH --gpus-per-node=2
 #SBATCH -t 00:30:00
-
+#SBATCH -e %x-%j.err 
+#SBATCH -o %x-%j.out 
 
 export ROOT_DIR=/home/benjamic
 export REPO_DIR=$ROOT_DIR/life-sequencing-dutch
@@ -20,5 +21,5 @@ source $VENV
 cd $REPO_DIR/src/llm/
 
 #Start training
-python -m src.new_code.pretrain --accelerator gpu --ddpstrategy gloo --device 2 --config projects/dutch_real/pretrain_cfg.json
+python -m src.new_code.pretrain --accelerator gpu --ddpstrategy gloo --device $SLURM_GPUS_ON_NODE --config projects/dutch_real/pretrain_cfg.json
 
