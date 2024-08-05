a3297cee84cd1d885244ebbadfad7f507fdab747
Flavio Hafner <hafner.flavio@gmail.com>
2024-08-05 14:36:07 +0200

    separate script for modules; update docs

--- requirements/load_venv.sh
+++ requirements/load_venv.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # This is how the venv on snellius (OSSC and regular) should be activated
 
-declare ENV_NAME="ossc_env_may2"
+declare ENV_NAME="ossc_env_aug2024"
 
-module purge 
-module load Python/3.11.3-GCCcore-12.3.0
-module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
-module load matplotlib/3.7.2-gfbf-2023a
+source snel_modules_2023.sh
 source ${ENV_NAME}/bin/activate
