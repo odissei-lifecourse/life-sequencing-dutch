64b2bd6b30b1501eb0ae46e7df1b01d75b34c799
Flavio Hafner <hafner.flavio@gmail.com>
2024-08-06 20:30:35 +0200

    update pretrain multigpu

--- src/llm/slurm_scripts/pretrain_multigpu_singlenode.sh
+++ src/llm/slurm_scripts/pretrain_multigpu_singlenode.sh
@@ -3,6 +3,7 @@
 #SBATCH --job-name=pretrain
 #SBATCH -p gpu
 #SBATCH --gpus-per-node=4
+#SBATCH --ntasks-per-node=4
 #SBATCH -t 00:30:00
 #SBATCH -e %x-%j.err 
 #SBATCH -o %x-%j.out 
@@ -12,7 +13,7 @@ export REPO_DIR=$ROOT_DIR/repositories/life-sequencing-dutch
 export VENV=$REPO_DIR/.venv/bin/activate
 
 #load the modules
-source $REPO_DIR/requirements/2023_snel_modules.sh
+source $REPO_DIR/requirements/snel_modules_2023.sh
 
 #source the virtual environment
 source $VENV
@@ -21,9 +22,9 @@ source $VENV
 cd $REPO_DIR/src/llm/
 
 #Start training
-python -m src.new_code.pretrain \
+srun python -m src.new_code.pretrain \
        --accelerator gpu \
-       --ddpstrategy gloo \
+       --ddpstrategy auto \
        --device $SLURM_GPUS_ON_NODE \
        --batch 256 \
        --hparams src/new_code/regular_hparams_large.txt \
