90d4348d1c44b3c166030f68632d75771ae09b9a
Flavio Hafner <hafner.flavio@gmail.com>
2024-08-21 13:00:32 +0200

    update slurm params

--- pop2vec/fake_data/slurm_scripts/create_embeddings.sh
+++ pop2vec/fake_data/slurm_scripts/create_embeddings.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
 #SBATCH --nodes=1
-#SBATCH --time=03:00:00
-#SBATCH --mem=300GB
+#SBATCH --time=02:00:00
+#SBATCH --mem=120GB
 #SBATCH -p fat_rome 
 #SBATCH -e %x-%j.err
 #SBATCH -o %x-%j.out
