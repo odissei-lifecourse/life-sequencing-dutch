2709eac08de45b394139dde7e525861c0ed6ee82
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-09-26 17:31:12 +0200

    update slurm script

--- pop2vec/evaluation/slurm_scripts/baseline.sh
+++ pop2vec/evaluation/slurm_scripts/baseline.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #
-#SBATCH --job-name=generate_income_baseline_snellius
-#SBATCH --ntasks-per-node=8
+#SBATCH --job-name=base_eval
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
 #SBATCH --nodes=1
 #SBATCH --time=1:00:00
-#SBATCH --mem=50G
+#SBATCH --mem=15G
 #SBATCH -e logs/%x.%j.err
 #SBATCH -o logs/%x.%j.out
 
-echo "job started" 
+echo "job started"
 
 data_dir="/projects/0/prjs1019/data/"
 date
 source requirements/load_venv.sh
 time python -m pop2vec.evaluation.baseline_evaluation_v1 $data_dir
 
-echo "job ended" 
+echo "job ended"
