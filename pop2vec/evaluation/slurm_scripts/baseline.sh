301fac43119dded45ff41e3fa8beb048848f4c00
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-09-27 15:23:34 +0200

    update slurm script, use job array

--- pop2vec/evaluation/slurm_scripts/baseline.sh
+++ pop2vec/evaluation/slurm_scripts/baseline.sh
@@ -6,14 +6,28 @@
 #SBATCH --nodes=1
 #SBATCH --time=1:00:00
 #SBATCH --mem=15G
-#SBATCH -e logs/%x.%j.err
-#SBATCH -o logs/%x.%j.out
+#SBATCH --array=1-2
+#SBATCH -e logs/%x.%A_%a.err
+#SBATCH -o logs/%x.%A_%a.out
 
 echo "job started"
 
 data_dir="/projects/0/prjs1019/data/"
+
+# assumes you're in the project repository
+config="pop2vec/evaluation/slurm_scripts/base_eval_config.txt"
+
+train_only=$(awk -v ArrayTaskID="$SLURM_ARRAY_TASK_ID" '$1==ArrayTaskID {print $3}' $config)
+
+
 date
 source requirements/load_venv.sh
-time python -m pop2vec.evaluation.baseline_evaluation_v1 $data_dir
 
+if [ "$train_only" -eq "1" ]; then
+    time python -m pop2vec.evaluation.baseline_evaluation_v1 --data-dir $data_dir --train-only
+elif [ "$train_only" -eq "0" ]; then
+    time python -m pop2vec.evaluation.baseline_evaluation_v1 --data-dir $data_dir
+fi
+
+    
 echo "job ended"
