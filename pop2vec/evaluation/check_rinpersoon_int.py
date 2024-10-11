e169a641d311f8fbee2e20c3cf72d5f3d02f3b06
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-11 15:19:27 +0200

    fix typos

--- pop2vec/evaluation/check_rinpersoon_int.py
+++ pop2vec/evaluation/check_rinpersoon_int.py
@@ -4,12 +4,12 @@ data_dir = "/gpfs/ostor/ossc9424/homedir/data/"
 background_path = data_dir + "llm/raw/background.csv"
 
 
-df_str = pd.read_csv(background_path, dtype={"RINPERSOON": "object"}, usecols="RINPERSOON")
-df_int = pd.read_csv(background_path, dtype={"RINPERSOON": "int"}, usecols="RINPERSOON")
+df_str = pd.read_csv(background_path, dtype={"RINPERSOON": "object"}, usecols=["RINPERSOON"])
+df_int = pd.read_csv(background_path, dtype={"RINPERSOON": "int"}, usecols=["RINPERSOON"])
 
-list1 = list(df_str["col1"].unique())
+list1 = list(df_str["RINPERSOON"].unique())
 set1 = set([int(v) for v in list1])
-set2 = set(df_int["col2"].unique())
+set2 = set(df_int["RINPERSOON"].unique())
 
 assert set1 == set2, "the two unique sets are not equal"
 assert len(set1) == len(df_str), f"all RINPERSOON IDs are not unique. df size = {len(df)}, set size = {len(set1)}"
