3a961dfd16bc9cb725afcbc9ea5bedfc8317749f
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-15 15:29:33 +0200

    update load_venv

--- requirements/load_venv.sh
+++ requirements/load_venv.sh
@@ -2,6 +2,9 @@
 # This is how the venv on regular snellius should be activated. For OSSC, change the ENV_NAME if needed.
 
 declare ENV_NAME=".venv"
+# on OSSC
+# declare ENV_NAME="/gpfs/ostor/ossc9424/homedir/virtual_envs/15oct2024/"
+
 
 source requirements/snel_modules_2023.sh
 source ${ENV_NAME}/bin/activate
