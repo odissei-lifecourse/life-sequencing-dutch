7b86ea68b4893fb968dcc5cd256243107b63dd28
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-16 12:41:04 +0200

    add numba dependency

--- requirements/snel_modules_2023.sh
+++ requirements/snel_modules_2023.sh
@@ -1,8 +1,9 @@
 
-module purge 
+module purge
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 module load matplotlib/3.7.2-gfbf-2023a
 module load h5py/3.9.0-foss-2023a
 module load dask/2023.9.2-foss-2023a
+module load numba/0.58.1-foss-2023a
