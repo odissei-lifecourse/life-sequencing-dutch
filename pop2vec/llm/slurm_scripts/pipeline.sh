367ba64a74970d502340800ac9c2af1b821e4987
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-31 16:01:34 +0100

    simplify pipeline script

--- pop2vec/llm/slurm_scripts/pipeline.sh
+++ pop2vec/llm/slurm_scripts/pipeline.sh
@@ -5,51 +5,15 @@
 #SBATCH --cpus-per-task 4 
 #SBATCH --nodes=1
 #SBATCH --time=02:30:00
-#SBATCH --mem=20G
+#SBATCH --mem=40G
 #SBATCH -p fat_rome
-#SBATCH -e %x-%j.err
-#SBATCH -o %x-%j.out
+#SBATCH -e logs/%x-%j.err
+#SBATCH -o logs/%x-%j.out
 
 
-# function to check if $2 is in $1
-stringContain() { case $2 in *$1* ) return 0;; *) return 1;; esac ;}
-
-# This function can be called with `source script.sh` -> fast initialization during interactive jobs
-initialize() {
-	# note that `declare` are local variables and will not be available out of function scope
-
-	if stringContain "ossc" $USER; then
-	    declare DATADIR="/gpfs/ostor/ossc9424/homedir/data"
-	else
-	    declare DATADIR="/projects/0/prjs1019"
-	fi 
-	
-	# add more users here as necessary
-	if stringContain "ossc" $USER; then
-	    declare ROOTDIR="/gpfs/ostor/ossc9424/homedir"
-	    declare VENV="$ROOTDIR/ossc_env"
-	elif stringContain "fhafner" $USER; then 
-	    declare ROOTDIR="/gpfs/home4/$USER"
-	    declare REPO_DIR="$ROOTDIR/repositories/life-sequencing-dutch"
-	    declare VENV="$REPO_DIR/.venv"
-	fi
-
-	source $REPO_DIR/requirements/2023_snel_modules.sh	
-	source "$VENV/bin/activate" 
-	
-	echo "job started" 
-	cd $REPO_DIR/src/llm/
-}
+source requirements/load_venv.sh
+srun python -m pop2vec.llm.src.new_code.pipeline pop2vec/llm/projects/dutch_real/pipeline_no_mlm_snellius_cfg.json
 
 
-if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-    initialize
-else
-    initialize
 
-    date
-    srun python -m pop2vec.llm.src.new_code.pipeline pop2vec/llm/projects/dutch_real/pipeline_cfg.json
-
-    echo "job ended"
-fi
 
