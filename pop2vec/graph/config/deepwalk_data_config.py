f56b374a5197e89dd9657aa75532ecb987468dea
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-10-31 12:03:00 +0100

    data config: fix typo; add mapping dir

--- pop2vec/graph/config/deepwalk_data_config.py
+++ pop2vec/graph/config/deepwalk_data_config.py
@@ -2,8 +2,10 @@ from pop2vec.utils.constants import DATA_ROOT
 
 data_config = {
     "parquet_root": DATA_ROOT + "graph/walks/",
-    "parquet_nests": "/*/*/*/*/*.parquet",
-    "walk_iteration_name": "walklen40_prob0.8",
+    "parquet_nests": "*/*/*/*/*.parquet",
+    "walk_iteration_name": "first_trial", 
+    #"walk_iteration_name": "walklen40_prob0.8",
     "embedding_dir": DATA_ROOT + "graph/embeddings/",
-    "model_dir": DATA_ROOT + "graph/mdoels/",
+    "model_dir": DATA_ROOT + "graph/models/",
+    "mapping_dir": DATA_ROOT + "graph/mappings/",
 }
