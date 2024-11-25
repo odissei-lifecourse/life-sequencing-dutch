de1015d1ac6e12edb883c42a91054f2cf2923e1f
tanzir5 <tanzir2430@gmail.com>
2024-11-25 11:02:21 -0500

    Create count_freq_vocab.py

---
+++ pop2vec/llm/src/new_code/count_freq_vocab.py
@@ -0,0 +1,27 @@
+import h5py
+import torch
+import numpy as np
+import pandas as pd
+
+vocab_df = pd.read_csv('?.csv')
+freq = {}
+for token in vocab_df['TOKEN']:
+  freq[token] = 0
+
+
+with h5py.File('?.h5', 'r') as hdf5:
+  original_sequence = torch.from_numpy(hdf5['original_sequence'][indices]).share_memory_()
+  for seq in original_sequence:
+    for sent in seq:
+      for token in sent:
+        if token in freq:
+          freq[token] += 1
+
+vocab_df['count'] = vocab_df['TOKEN'].map(freq)
+
+vocab_df = vocab_df.sort_values(by='count')
+print(vocab_df[:10])
+print("-"*10)
+print(vocab_df[-10:])
+vocab_df.to_csv('good_vocab_with_count.csv')
+
