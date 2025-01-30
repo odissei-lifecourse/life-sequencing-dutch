4a26d36a2cb666b7ee43de52f0f4fedd777e43b6
tanzir5 <tanzir2430@gmail.com>
2025-01-30 03:55:19 +0100

    fixes mean pooling issue

---
+++ pop2vec/llm/slurm_scripts/infer_snellius_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=infer_small
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=30:00
+#SBATCH --mem=1G
+#SBATCH -p gpu
+#SBATCH --gpus 1
+#SBATCH -e logs/%x-%j.err
+#SBATCH -o logs/%x-%j.out
+
+#declare PREFIX="/gpfs/ostor/ossc9424/homedir/"
+
+#export CUDA_VISIBLE_DEVICES=0
+
+echo "job started"
+
+source requirements/load_venv.sh
+
+date
+time python -m pop2vec.llm.src.new_code.infer_embedding pop2vec/llm/projects/dutch_real/infer_cfg_test_snellius.json
+
+echo "job ended successfully"
