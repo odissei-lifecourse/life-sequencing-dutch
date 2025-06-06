e5c11fc0c0e576a0d60a9af3544d540a61d66e94
tanzir5 <tanzir2430@gmail.com>
2025-06-06 07:05:55 +0200

    attempt continued

--- pop2vec/llm/slurm_scripts/pretrain_pt_gpu_1_test.sh
+++ pop2vec/llm/slurm_scripts/pretrain_pt_gpu_1_test.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=0:20:00
 #SBATCH --mem=80G
 #SBATCH --gpus=1
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu_a100
 #SBATCH -e /home/tislampial/logs/%x.%j.err
 #SBATCH -o /home/tislampial/logs/%x.%j.out
 
@@ -19,8 +19,8 @@ cd "$PROJECT_DIR"
 # ---------- modules / env ---------------------------------------------
 module purge
 module load 2022
-module load Python/3.10.4-GCCCore-11.3.0
-module load PyTorch/1.12.0-foss-CUDA-11.7.0
+module load Python/3.10.4-GCCcore-11.3.0
+module load PyTorch/1.12.0-foss-2022a
 module load SciPy-bundle/2022.05-foss-2022a
 module load matplotlib/3.5.2-foss-2022a
 source requirements/load_venv.sh 
@@ -30,7 +30,7 @@ source requirements/load_venv.sh
 CFG="pop2vec/llm/configs/Snellius/pretrain_pt_gpu_1_test.cfg"
 
 date
-time python -m pop2vec.llm.src.pytorch_port.pretrain \
+time python -m pop2vec.llm.src.new_code.pytorch_port.pretrain \
          --config "$CFG" \
          --val_check_interval 0.5 \
          --log_every 10 \
