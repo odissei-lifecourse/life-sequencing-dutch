9c969eae5ae07467b4facb157cacb11bbdf58378
tanzir5 <tanzir2430@gmail.com>
2025-07-11 13:15:37 +0200

    create parquet seq codes

--- pop2vec/llm/slurm_scripts/create_parquet_seq.sh
+++ pop2vec/llm/slurm_scripts/create_parquet_seq.sh
@@ -3,12 +3,19 @@
 #SBATCH --job-name=create_parquet_seq
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --time=01:30:00
-#SBATCH --mem=30G
+#SBATCH --cpus-per-task=7
+#SBATCH --time=24:30:00
+#SBATCH --mem=500G
 #SBATCH -p rome 
 #SBATCH -e logs/%x-%j.err
 #SBATCH -o logs/%x-%j.out
 
+
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export POLARS_MAX_THREADS=1
 
 echo "job started"
 
